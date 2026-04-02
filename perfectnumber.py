4c65f4c96c98c023e0b95fc1ff123110ade22eab
Praneeth999u <praneeth3200@gmail.com>
2026-04-02 10:10:03 +0530

    Add files via upload

---
+++ perfectnumber.py
@@ -0,0 +1,13 @@
+import math
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        sum = 1 
+        if num==1:
+            return False
+        for i in range(2,int(math.sqrt(num))+1 ):
+            if ( num % i ==0):
+                sum +=  i + num /i
+        if sum == num:
+            return True
+        else:
+            return False 
